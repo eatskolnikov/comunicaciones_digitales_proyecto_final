16a81b1e0dabdc9f99a4a981b36150bc750c8589
enmanueltoribiocorotos <enmanuel.toribio@corotos.com.do>
2024-04-09 01:02:05 -0400

    Changing from text to image

--- a_readInputFile.m
+++ a_readInputFile.m
@@ -1,20 +1,28 @@
-% Abrir el archivo en modo 'lectura'
-fileID = fopen('files/fileToSend.txt', 'r');
-% Leer el archivo como 'uint8' para obtener la representación numérica de cada carácter
+% Abrir el archivo de imagen en modo 'lectura'
+fileID = fopen('files/imagen.bmp', 'r');
+
+% Leer el archivo como 'uint8' para obtener la representación numérica de los píxeles
 data = fread(fileID, '*uint8');
+
 % Cerrar el archivo
 fclose(fileID);
+
 % Convertir los datos a su representación binaria
 % Cada 'uint8' se convierte a 8 bits, formando una secuencia de bits
 bitSequence = dec2bin(data, 8)';
+
 % Convertir la matriz de caracteres en una secuencia lineal de bits
 bitSequence = bitSequence(:);
+
 % Opcional: Convertir la secuencia de caracteres de bits a un vector numérico de 0s y 1s
 bitVector = bitSequence - '0';
-% Supongamos que bitVector es tu vector de bits
+
+% La longitud del vector de bits
 nBits = length(bitVector);
-% Crear un vector de tiempo, asumiendo 1 segundo entre cada bit (ajusta según sea necesario)
+
+% Crear un vector de tiempo, asumiendo 1 segundo entre cada bit (esto es solo un ejemplo,
+% deberías ajustar el paso de tiempo según la tasa de bits que desees)
 timeVector = (0:nBits-1)';
 
 % Combinar el vector de tiempo y los datos en una sola matriz
-simin = [timeVector, bitVector];
+simin = [timeVector, double(bitVector)];
--- c_readAndDecodeOutputFile.m
+++ c_readAndDecodeOutputFile.m
@@ -1,17 +1,24 @@
 load('files/receivedFileData.mat','received_binary_data');  % Carga sólo la variable 'binaryData'
 
-% Dividir la cadena en segmentos de 8 bits y convertirlos a texto
-textData = '';
-for i = 1:8:length(received_binary_data)-7
-    binSegment = received_binary_data(2, i:i+7);
-    binStr = num2str(binSegment);
-    binStr(binStr == ' ') = '';
-    textData = [textData, char(bin2dec(binStr))];
+% La información de la imagen está en la segunda dimensión
+image_binary_data = received_binary_data(2, :);
+
+% Inicializar un vector para almacenar los bytes de la imagen
+imageData = uint8([]);
+
+% Dividir la secuencia de bits en segmentos de 8 bits y convertirlos a bytes numéricos
+for i = 1:8:length(image_binary_data)-7
+    % Extraer un segmento de 8 bits
+    binSegment = image_binary_data(i:i+7);
+    % Convertir el segmento binario a un número decimal (byte)
+    byte = bin2dec(num2str(binSegment));
+    % Añadir el byte al vector de la imagen
+    imageData = [imageData, byte];
 end
 
-% Guardar el texto decodificado en un archivo .txt
-fileID = fopen('files/receivedFile.txt', 'w');
-fprintf(fileID, '%s', textData);
+% Guardar los datos de la imagen en un archivo .bmp
+fileID = fopen('files/receivedFile.bmp', 'wb');  % Notar el 'wb' para modo de escritura binaria
+fwrite(fileID, imageData, 'uint8');
 fclose(fileID);
 
-disp('El texto decodificado ha sido guardado en decodedText.txt');
+disp('La imagen decodificada ha sido guardada en receivedFile.bmp');
